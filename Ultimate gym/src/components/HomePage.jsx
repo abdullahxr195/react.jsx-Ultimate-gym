87671f860a132f0c8c160d3cd9f0eb1792350e1c
abdullahxr65-ops <abdullahxr195@gmail.com>
2026-03-14 12:24:15 +0300

    hgh

---
+++ Ultimate gym/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+
+
+export default function HomePage() {
+    return (
+        <>
+        <div>
+            <h1>Welcome to Ultimate Gym!</h1>
+            <p>Your fitness journey starts here. Explore our classes, trainers, and facilities to achieve your health goals.</p>
+        </div>
+        </>
+    );
+}
