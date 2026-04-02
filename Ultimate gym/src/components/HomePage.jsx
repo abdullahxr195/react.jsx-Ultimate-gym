660d922547f73bffec45e6d52faab8aa9ec87c48
abdullahxr65-ops <abdullahxr195@gmail.com>
2026-04-02 13:59:05 +0300

    ..

--- Ultimate gym/src/components/HomePage.jsx
+++ Ultimate gym/src/components/HomePage.jsx
@@ -1,12 +1,33 @@
 
-
+import {Card , CardActionArea, CardMedia, CardContent , Typography ,Button} from '@mui/material';
 export default function HomePage() {
     return (
         <>
-        <div>
-            <h1>Welcome to Ultimate Gym!</h1>
-            <p>Your fitness journey starts here. Explore our classes, trainers, and facilities to achieve your health goals.</p>
-        </div>
+          <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
         </>
+        
     );
 }
